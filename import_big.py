caddb2f0e00558013516c774ce09921d15082012
obby <freesource@freesource.com>
2017-04-15 05:58:30 -0700

    add big import

--- import_big.py
+++ import_big.py
@@ -1,34 +1,35 @@
 import codecs
 import sys
 import time
+import multiprocessing
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1', 27017)
-db = client.admin
-print('Signed in: {0}'.format(db.authenticate('admin', '.gpe7h+99W:P}gU}')))
-data = db['data']
-
-def pushLines():
+def pushLines(lines):
 		global lines
 		data.insert_many(lines)
 
+if __name__ == "__main__":
+	client = MongoClient('mongodb://127.0.0.1', 27017)
+	db = client.admin
+	print('Signed in: {0}'.format(db.authenticate('admin', '.gpe7h+99W:P}gU}')))
+	data = db['data']
 
-lines = []
+	lines = []
 
-for line in codecs.open(sys.argv[1], encoding='utf-8', errors='ignore'):
-		try:
-				if len(lines) >= 1000000:
-					print('Pushing 1000000')
-					pushLines()
-					lines = []
-				email, password = line.strip().rstrip().split(':')
-				lines.append({'username': None, 'password': password, 'email': email, 'database': 'linkedin', 'ip': None})
-		except:
-				continue
+	for line in codecs.open(sys.argv[1], encoding='utf-8', errors='ignore'):
+			try:
+					if len(lines) >= 1000000:
+						print('Pushing 1000000')
+						multiprocessing.Process(target=pushLines, args=lines).start()
+						lines = []
+					email, password = line.strip().rstrip().split(':')
+					lines.append({'username': None, 'password': password, 'email': email, 'database': 'linkedin', 'ip': None})
+			except:
+					continue
 
-print('Pushing leftover...')
-pushLines()
-lines = []
+	print('Pushing leftover...')
+	pushLines()
+	lines = []
 
 #data.insert_one({'username': 'test', 'password': 'testPassword', 'email': 'test@test.com', 'database': 'myspaceTest', 'ip': '127.0.0.1'})
