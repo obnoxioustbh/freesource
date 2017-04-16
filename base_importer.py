7cedf861e4149cad91f245abac27aa3abfe116dc
obby <freesource@freesource.com>
2017-04-16 16:59:52 -0700

    lol

--- base_importer.py
+++ base_importer.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 sys.argv = ['', 'dbs/xsplit.txt']
 
-client = MongoClient('mongodb://93.190.142.215', 27017)
+client = MongoClient('mongodb://127.0.0.1', 27017)
 db = client.admin
 print('Signed in: {0}'.format(db.authenticate('admin', '.gpe7h+99W:P}gU}')))
 data = db['data']
@@ -18,11 +18,14 @@ file = codecs.open(sys.argv[1], encoding='utf-8', errors='ignore').readlines()
 lines = []
 
 for line in file:
-	if len(lines) >= 50:
-		print('pushing lines..')
-		lines = []
-		#pushLines()
-	username, name, email, password = line.strip().rstrip().split('\t')
-	lines.append({'username': username, 'password': password, 'email': email, 'database': 'xsplit', 'ip': '127.0.0.1'})
+	try:
+		username, name, email, password = line.strip().rstrip().split('\t')
+		lines.append({'username': username, 'password': password, 'email': email, 'database': 'xsplit', 'ip': '127.0.0.1'})
+	except:
+		continue
+		
+print('Pushing..')
+pushLines()
+print('Pushed..')
 
 #data.insert_one({'username': 'test', 'password': 'testPassword', 'email': 'test@test.com', 'database': 'myspaceTest', 'ip': '127.0.0.1'})
