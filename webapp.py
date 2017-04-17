5a64361f333806ef27aa130bd7bdbe4812aea5c2
obby <freesource@freesource.com>
2017-04-17 02:18:31 -0700

    huge update

--- webapp.py
+++ webapp.py
@@ -6,6 +6,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+def tableCreator(ths):
+	baseString = '<table class="table table-inverse"><tbody>'
+	for key, value in ths.items():
+		baseString += '<tr><th>{0}</th><td>{1}</td></tr>'.format(key, value)
+	baseString += '</tbody></table>'
+	return baseString
+
 app = Flask(__name__)
 client = MongoClient('mongodb://93.190.142.215', 27017)
 db = client.admin
@@ -21,18 +28,30 @@ def freesource():
 	#return 'REBUILDING INDEXES, I WANNA SLEEP THO SO WE GO UP TOMORROW IG'
 	lookupData = ''
 	lookup = False
-	opts = {'username': request.args.get('username'), 'email': request.args.get('email'), 'ip': request.args.get('ip'), 'password': request.args.get('password')}
-	for key, value in opts.items():
-		if opts[key]:
-			lookup = {'type': key, 'data': opts[key]}
+
+	content = request.args.get('content')
+
+	if content:
+
+		if '127.0.0.1' in content:
+			return 'fuck off broooo'
+
+		if '@' in content:
+				lookup = {'type': 'email', 'data': content}
+
+		elif re.search(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', content):
+			lookup = {'type': 'ip', 'data': content}
+
+		else:
+			lookup = {'type': 'username', 'data': content}
 
 	if lookup != False:
 		lookup['data'] = lookup['data']
 		for result in doLookup(lookup):
-			for key, value in result.items():
-				lookupData += '<p>{0}: {1}<br/></p>\r\n'.format(key, value)
-			lookupData += "--------------------------------------<br/>"
-			#lookupData += '{0}<br/>\r\n'.format(str(result))
+			baseData = ''
+			table = tableCreator(result)
+			baseData += table
+			lookupData += baseData
 	else:
 		lookupData = "<p>Ready.</p>"
 
